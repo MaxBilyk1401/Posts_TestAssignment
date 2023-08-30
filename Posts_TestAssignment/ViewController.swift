24fe1f7383aff6f8094a1982b7f59b6e0cf12b75
Maksym Bilyk <black.bro1401@gmail.com>
2023-08-30 19:30:14 +0300

    Initial Commit

---
+++ Posts_TestAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Posts_TestAssignment
+//
+//  Created by Maxym Bilyk on 30.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
